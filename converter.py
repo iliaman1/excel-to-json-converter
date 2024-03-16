85010fddcba89ff91a6aca283a79e018e8585edf
iliaman <iliaman1993@gmail.com>
2024-03-16 22:32:34 +0300

    after code review

--- converter.py
+++ converter.py
@@ -1,9 +1,14 @@
 import datetime
 import json
+import logging
 from dataclasses import dataclass
-from pprint import pprint
 
 import openpyxl
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s')
+
+DATA_START_ROW = 5
+DATA_OFFSET = 9
 
 
 @dataclass
@@ -27,49 +32,67 @@ class RawData:
     address: str
     months: list[Month]
 
+    def get_full_name(self) -> tuple[str, str, str]:
+        values = self.full_name.strip().split(' ')
+
+        if len(values) != 3:
+            logging.warning(
+                f'Для "{self.full_name.strip()}" указано не полное ФИО. '
+                'Будут использованы пустые значения.'
+            )
+
+        match len(values):
+            case 1:
+                return values[0], '', ''
+            case 2:
+                return values[0], values[1], ''
+            case 3:
+                return values[0], values[1], values[2]
+            case _:
+                return '', '', ''
+
     def generate_tar4(self) -> list[dict]:
-        tar4 = []
-        for index in range(12):
-            tar4.append({
+        return [
+            {
                 'nmonth': index + 1,
                 'nsummonth': self.months[index].d_201,
                 'tar4sum': [{'ncode': 201, 'nsum': self.months[index].d_201}]
-            })
-        return tar4
+            }
+            for index in range(12)
+        ]
 
     def generate_tar7(self) -> list[dict]:
-        tar7 = []
-        for index in range(12):
-            tar7.append({
+        return [
+            {
                 'nmonth': index + 1,
                 'nsummonth': self.months[index].b_600,
                 'tar7sum': [{'ncode': 600, 'nsum': self.months[index].b_600}]
-            })
-        return tar7
+            }
+            for index in range(12)
+        ]
 
     def generate_tar14(self) -> list[dict]:
-        tar14 = []
-        for index in range(12):
-            tar14.append({
+        return [
+            {
                 'nmonth': index + 1,
                 'nsumdiv': 0,
                 'nsumt': self.months[index].tax
-            })
-        return tar14
+            }
+            for index in range(12)
+        ]
 
-    def serialize_to_dict(self):
-        fio = self.full_name.split()
-        if len(fio) < 3:
-            fio += ['', '', '']
+    def to_dict(self):
+        last_name, first_name, surname = self.get_full_name()
+
         return {
             'docagentinfo': {
                 'cln': self.personal_number,
                 'cstranf': '112',  # 2.5 Код страны гражданства (подданства)
                 'cvdoc': '01',  # 2.6 Код документа, удостоверяющего личность
                 'nrate': 13,  # 3. Размер ставки подоходного налога с физических лиц, проценты
-                'vfam': fio[0],
-                'vname': fio[1],
-                'votch': fio[2]
+                'vfam': last_name,
+                'vname': first_name,
+                'votch': surname
             },
             'nsumstand': sum([nalog.b_600 for nalog in self.months]),
             'ntsumbank': 0,
@@ -91,75 +114,68 @@ class RawData:
         }
 
 
+# TODO: перенести функции в класс
+class Converter:
+    ...
+
+
 def create_raw_data_list(excel_filename: str) -> list[RawData]:
     data: list[RawData] = []
     wb = openpyxl.load_workbook(excel_filename)
     sheet = wb.active
     max_rows = sheet.max_row
 
-    for xlsx_row in range(5, max_rows + 1, 9):
+    for xlsx_row in range(DATA_START_ROW, max_rows + 1, DATA_OFFSET):
         months = []
+        # TODO: перенести в константы 2 и 14
         for month in range(2, 14):
             months.append(
                 Month(
-                    sheet.cell(row=xlsx_row + 1, column=month).value if sheet.cell(row=xlsx_row + 1,
-                                                                                   column=month).value else 0,
-                    sheet.cell(row=xlsx_row + 2, column=month).value if sheet.cell(row=xlsx_row + 2,
-                                                                                   column=month).value else 0,
-                    sheet.cell(row=xlsx_row + 3, column=month).value if sheet.cell(row=xlsx_row + 3,
-                                                                                   column=month).value else 0,
-                    sheet.cell(row=xlsx_row + 4, column=month).value if sheet.cell(row=xlsx_row + 4,
-                                                                                   column=month).value else 0,
-                    sheet.cell(row=xlsx_row + 5, column=month).value if sheet.cell(row=xlsx_row + 5,
-                                                                                   column=month).value else 0,
-                    sheet.cell(row=xlsx_row + 6, column=month).value if sheet.cell(row=xlsx_row + 6,
-                                                                                   column=month).value else 0,
-                    sheet.cell(row=xlsx_row + 7, column=month).value if sheet.cell(row=xlsx_row + 7,
-                                                                                   column=month).value else 0,
-                    sheet.cell(row=xlsx_row + 8, column=month).value if sheet.cell(row=xlsx_row + 8,
-                                                                                   column=month).value else 0
+                    # TODO: перенести 8 в константы
+                    *[sheet.cell(row=xlsx_row + index + 1, column=month).value or 0 for index in range(8)]
                 )
             )
 
         data.append(
             RawData(
-                sheet.cell(row=xlsx_row, column=1).value,
-                sheet.cell(row=xlsx_row, column=2).value,
-                sheet.cell(row=xlsx_row, column=6).value,
-                sheet.cell(row=xlsx_row, column=8).value,
-                sheet.cell(row=xlsx_row, column=10).value,
+                # TODO: перенести (1, 2, 6, 8, 10) в константы
+                *[sheet.cell(row=xlsx_row, column=i).value for i in (1, 2, 6, 8, 10)],
                 months
             )
         )
+
     return data
 
 
-rawdata = create_raw_data_list('доход2023.xlsx')
-
-
-def serialize_to_json(rawdata: list[RawData]):
+def to_dict(rawdata: list[RawData]) -> dict:
     current_time = datetime.datetime.now()
     formatted_time = current_time.strftime("%Y-%m-%dT%H:%M:%S")
-    return {'pckagent': {
-        'docagent': [person.serialize_to_dict() for person in rawdata],
-        'pckagentinfo': {
-            'dcreate': formatted_time,
-            'ngod': current_time.year,
-            'nmns': 741,
-            'nmnsf': 741,
-            'ntype': 1,
-            'vexec': 'Буйко Т.С.',
-            'vphn': '72-30-50',
-            'vunp': '700069297'
-        }}}
+
+    return {
+        'pckagent': {
+            'docagent': [person.to_dict() for person in rawdata],
+            'pckagentinfo': {
+                'dcreate': formatted_time,
+                'ngod': current_time.year,
+                'nmns': 741,
+                'nmnsf': 741,
+                'ntype': 1,
+                'vexec': 'Буйко Т.С.',
+                'vphn': '72-30-50',
+                'vunp': '700069297'
+            }
+        }
+    }
 
 
-def generate_filename(unp, form_type, department_code, part_number=None):
+def generate_filename(unp, form_type, department_code, part_number=None) -> str:
     current_time = datetime.datetime.now()
     formatted_time = current_time.strftime("%Y%m%d%H%M%S")
     filename = f"D{unp}_{current_time.year}_{form_type}_{department_code}_{formatted_time}"
+
     if part_number is not None:
         filename += f"_{part_number:04d}"
+
     filename += ".json"
     return filename
 
@@ -171,27 +187,24 @@ def batch(iterable, n=1):
 
 
 def make_files(data: list[RawData]):
+    # TODO: вынести в константы УНП И максимальное колл записей(200)
     unp = 700069297
+    pages_count = len(data) // 200
 
-    part_counts = len(data) // 200
-    if part_counts == 0:
-        data = serialize_to_json(data)
-        with open(
-                f'gen_json/{generate_filename(unp, 1, 0)}',
-                'w',
-                encoding='utf-8'
-        ) as write_file:
-            json.dump(data, write_file, indent=4, ensure_ascii=False)
-        return
+    if pages_count > 0:
+        for part, group in enumerate(batch(data, 200)):
+            part_data = to_dict(group)
+            filename = f'gen_json/{generate_filename(unp, 1, 0, part + 1)}'
 
-    for part, group in enumerate(batch(data, 200)):
-        partdata = serialize_to_json(group)
-        with open(
-                f'gen_json/{generate_filename(unp, 1, 0, part + 1)}',
-                'w',
-                encoding='utf-8'
-        ) as write_file:
-            json.dump(partdata, write_file, indent=4, ensure_ascii=False)
+            with open(filename, 'w', encoding='utf-8') as file:
+                json.dump(part_data, file, indent=4, ensure_ascii=False)
+    else:
+        data = to_dict(data)
+        filename = f'gen_json/{generate_filename(unp, 1, 0)}'
+
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
 
 
-make_files(rawdata)
+if __name__ == '__main__':
+    make_files(create_raw_data_list('доход2023.xlsx'))
